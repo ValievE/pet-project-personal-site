10fcd6a3a10c9dd372baf564a245493e8999c05d
Emil Valiev <evaliev90@gmail.com>
2024-03-25 17:32:32 +0500

    commit3

--- src/components/section2/section2.vue
+++ src/components/section2/section2.vue
@@ -2,8 +2,18 @@
   <div class="section sec2">
     <h2 class="title_Medium">Fields I work for</h2>
     <div class="sec2__buttonss">
-      <div class="sec2__button" v-for="nameButton in sec2ButtonsInfo" :key="i">
-        <button class="button sec2__button-item">{{ nameButton }}</button>
+      <div
+        class="sec2__button"
+        v-for="(nameButton, i) of sec2ButtonsInfo"
+        :key="i"
+      >
+        <button
+          class="button sec2__button-item"
+          :class="{ 'button button_active sec2__button-item': ind === i }"
+          @click="ind = i"
+        >
+          {{ nameButton }}
+        </button>
       </div>
     </div>
     <div class="sec2_1">
@@ -33,22 +43,39 @@
         </div>
       </div>
       <div class="sec2_2">
-        <Section2__Card title="assa" />
+        <div
+          class="section2__text"
+          v-for="(cardInfoitem, i) in cardInfo[ind]"
+          :key="i"
+        >
+          <p class="section2__text__title">
+            {{ cardInfoitem.title }}
+          </p>
+          <p class="section2__text__paragraph">
+            {{ cardInfoitem.text }}
+          </p>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Section2__Card from "./section2-card/section2-card.vue";
+import cardsInfo from "./section2.json";
 export default {
-  components: { Section2__Card },
   data() {
     return {
+      ind: 0,
       sec2ButtonsInfo: ["Concept", "Mechanics", "Graphics"],
+      cardInfo: cardsInfo,
     };
   },
   props: {},
+  watch: {
+    cardInfoitem(value) {
+      console.log(value, "value");
+    },
+  },
 };
 </script>
 
